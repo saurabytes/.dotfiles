04ed1c84f46e902c758446e6ffd5ccf066ce1f86
Saurabh Agrawal <saurabytes@gmail.com>
2023-05-11 22:12:57 -0400

    Dotfiles

---
+++ ssh/.ssh/ssh.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+if [ "$1" = "" ]
+then
+  echo "Usage: $0 <email_address>"
+  exit
+fi
+
+echo "Generating a new SSH key for GitHub..."
+
+# Generating a new SSH key
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#generating-a-new-ssh-key
+ssh-keygen -t ed25519 -C $1 -f ~/.ssh/github_key
+
+# Adding your SSH key to the ssh-agent
+# https://docs.github.com/en/github/authenticating-to-github/generating-a-new-ssh-key-and-adding-it-to-the-ssh-agent#adding-your-ssh-key-to-the-ssh-agent
+eval "$(ssh-agent -s)"
+
+echo "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/github_key" >> ~/.ssh/config
+
+ssh-add -K ~/.ssh/github_key
+
+# Adding your SSH key to your GitHub account
+# https://docs.github.com/en/github/authenticating-to-github/adding-a-new-ssh-key-to-your-github-account
+echo "run 'pbcopy < ~/.ssh/github_key.pub' and paste that into GitHub"
